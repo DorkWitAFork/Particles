5fbcb36d406e7fd77d1d776584efdeb25d5a3cd6
DorkWitAFork <75294127+DorkWitAFork@users.noreply.github.com>
2023-12-06 11:12:53 -0800

    Update Matrices.cpp

--- code/Matrices.cpp
+++ code/Matrices.cpp
@@ -2,130 +2,131 @@
 #include <iostream>
 
 using namespace std;
-using namespace Matrices;
-
-Matrix::Matrix(int _rows, int _cols)
+namespace Matrices
 {
-	rows = _rows;
-	cols = _cols;
-	a.resize(rows);
-
-	for (int i = 0; i < rows; i++)
+	Matrix::Matrix(int _rows, int _cols)
 	{
-		a.at(i).resize(cols, 0);
-	}
-}
+		rows = _rows;
+		cols = _cols;
+		a.resize(rows);
 
-Matrix operator+(const Matrix& a, const Matrix& b)
-{
-	Matrix c(a.getRows(), a.getCols());
-	if (a.getRows() != b.getRows() || a.getCols() != b.getCols()) {
-		throw std::runtime_error("Error: dimensions must agree");
-	}
-	for (int i = 0; i < a.getRows(); i++)
-	{
-		for (int j = 0; j < a.getCols(); j++)
+		for (int i = 0; i < rows; i++)
 		{
-			c(i, j) = a(i, j) + b(i, j);
+			a.at(i).resize(cols, 0);
 		}
 	}
-	return c;
-}
-Matrix operator*(const Matrix& a, const Matrix& b)
-{
-	if (a.getCols() != b.getRows())
-	{
-		throw runtime_error("Error: dimensions must agree");
-	}
-	Matrix c(a.getRows(), b.getCols());
 
-	for (int i = 0; i < a.getRows(); i++)
+	Matrix operator+(const Matrix& a, const Matrix& b)
 	{
-		for (int j = 0; j < b.getCols(); j++)
+		Matrix c(a.getRows(), a.getCols());
+		if (a.getRows() != b.getRows() || a.getCols() != b.getCols()) {
+			throw std::runtime_error("Error: dimensions must agree");
+		}
+		for (int i = 0; i < a.getRows(); i++)
 		{
-			for (int k = 0; k < a.getCols(); k++)
+			for (int j = 0; j < a.getCols(); j++)
 			{
-				c(i, j) += a(i, k) * b(k, j);
+				c(i, j) = a(i, j) + b(i, j);
 			}
 		}
+		return c;
 	}
-
-	return c;
-
-}
-
-bool operator==(const Matrix& a, const Matrix& b)
-{
-
-	if (a.getRows() != b.getRows() || a.getCols() != b.getCols())
+	Matrix operator*(const Matrix& a, const Matrix& b)
 	{
-		return false;
-	}
-
-
-	for (int i = 0; i < a.getRows(); i++)
-	{
-		for (int j = 0; j < a.getCols(); j++)
+		if (a.getCols() != b.getRows())
 		{
-			if (abs(a(i, j) - b(i, j)) > 0.001)
+			throw runtime_error("Error: dimensions must agree");
+		}
+		Matrix c(a.getRows(), b.getCols());
+
+		for (int i = 0; i < a.getRows(); i++)
+		{
+			for (int j = 0; j < b.getCols(); j++)
 			{
-				return false;
+				for (int k = 0; k < a.getCols(); k++)
+				{
+					c(i, j) += a(i, k) * b(k, j);
+				}
 			}
 		}
+
+		return c;
+
 	}
 
-	return true;
-}
-
-
-bool operator!=(const Matrix& a, const Matrix& b)
-{
-	return !(a == b);
-
-}
-
-
-ostream& operator<<(ostream& os, const Matrix& a)
-{
-	for (int i = 0; i < a.getRows(); i++)
+	bool operator==(const Matrix& a, const Matrix& b)
 	{
-		for (int j = 0; j < a.getCols(); j++)
+
+		if (a.getRows() != b.getRows() || a.getCols() != b.getCols())
 		{
-			os << setw(10) << right << a(i, j);
+			return false;
 		}
-		os << endl;
+
+
+		for (int i = 0; i < a.getRows(); i++)
+		{
+			for (int j = 0; j < a.getCols(); j++)
+			{
+				if (abs(a(i, j) - b(i, j)) > 0.001)
+				{
+					return false;
+				}
+			}
+		}
+
+		return true;
 	}
-	return os;
-}
 
 
-RotationMatrix::RotationMatrix(double theta) : Matrix(2, 2)
-{
-	a.at(0).at(0) = cos(theta);
-	a.at(0).at(1) = -sin(theta);
-	a.at(1).at(0) = sin(theta);
-	a.at(1).at(1) = cos(theta);
-
-
-}
-
-ScalingMatrix::ScalingMatrix(double scale) : Matrix(2, 2)
-{
-	a.at(0).at(0) = scale;
-	a.at(1).at(1) = scale;
-	a.at(0).at(1) = 0;
-	a.at(1).at(0) = 0;
-
-}
-
-
-
-
-TranslationMatrix::TranslationMatrix(double xShift, double yShift, int nCols) : Matrix(2, nCols)
-{
-	for (int i = 0; i < nCols; i++)
+	bool operator!=(const Matrix& a, const Matrix& b)
 	{
-		a.at(0).at(i) = xShift;
-		a.at(1).at(i) = yShift;
+		return !(a == b);
+
+	}
+
+
+	ostream& operator<<(ostream& os, const Matrix& a)
+	{
+		for (int i = 0; i < a.getRows(); i++)
+		{
+			for (int j = 0; j < a.getCols(); j++)
+			{
+				os << setw(10) << right << a(i, j);
+			}
+			os << endl;
+		}
+		return os;
+	}
+
+
+	RotationMatrix::RotationMatrix(double theta) : Matrix(2, 2)
+	{
+		a.at(0).at(0) = cos(theta);
+		a.at(0).at(1) = -sin(theta);
+		a.at(1).at(0) = sin(theta);
+		a.at(1).at(1) = cos(theta);
+
+
+	}
+
+	ScalingMatrix::ScalingMatrix(double scale) : Matrix(2, 2)
+	{
+		a.at(0).at(0) = scale;
+		a.at(1).at(1) = scale;
+		a.at(0).at(1) = 0;
+		a.at(1).at(0) = 0;
+
+	}
+
+
+
+
+	TranslationMatrix::TranslationMatrix(double xShift, double yShift, int nCols) : Matrix(2, nCols)
+	{
+		for (int i = 0; i < nCols; i++)
+		{
+			a.at(0).at(i) = xShift;
+			a.at(1).at(i) = yShift;
+		}
 	}
 }
